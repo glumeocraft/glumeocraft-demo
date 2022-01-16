ebe19827a7fae98b52da68f84eeb189d647f62c5
a.gida <a.gida@mortoff.hu>
2022-01-16 11:47:18 +0100

    add battle script

---
+++ Assets/Scripts/Battle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Battle : MonoBehaviour
+{
+
+    public bool isSwamp;
+    public BattleHelper battleHelper;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isSwamp = false;
+        DoBattle();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DoBattle()
+    {
+        Debug.Log("battle");
+    }
+}
