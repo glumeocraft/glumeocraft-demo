ffe7b711e73e6a59f9ccc19b11315a9617d3efb6
RCINET\doram <dora@marton.nexius.hu>
2022-02-01 19:34:56 +0100

    added a second class

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -9,17 +9,30 @@ public class Battle : MonoBehaviour
     public bool isSwamp;
     public BattleHelper battleHelper;
 
+
+
+
+    
+
+
+
+    public Battle()
+    {
+        battleHelper = new BattleHelper();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         isSwamp = false;
         DoBattle();
+          
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void DoBattle()
